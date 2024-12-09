9e7f83b2e9f5b9d1632e3b4e781ed55913634865
Michał Szot <148005035+Mihchi99@users.noreply.github.com>
2024-12-09 20:12:41 +0100

    3ParticleEfekty
    Wióry z różdżki i pocisków, efekt przy wciśnięciu przycisku strzału

---
+++ Assets/Wand.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wand : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 11;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        }
+    }
+}
